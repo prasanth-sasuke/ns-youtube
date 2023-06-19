5768008d5c89113918bb8a095c70d345b2e12fcc
prasanth-CN <lakshmi.prasanth@codenatives.com>
2023-06-19 12:03:08 +0530

    Initial commit

---
+++ src/components/VideoDetial.jsx
@@ -0,0 +1,60 @@
+import { useState,useEffect } from 'react';
+import { Link,useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography,Box,Stack, colors } from '@mui/material';
+import { CheckCircle, FeaturedPlayList } from '@mui/icons-material';
+
+import { Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromApi';
+import { ClassNames } from '@emotion/react';
+
+const VideoDetial = () => {
+  const [VideoDetial, setVideoDetial] = useState(null)
+  const [videos, setVideos] = useState([])
+  const { id } = useParams();
+
+  useEffect(()=>{
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+    .then((data)=> setVideoDetial(data.items[0]));
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+    .then((data)=> setVideos(data.items))
+  },[id])
+
+  if(!VideoDetial?.snippet) return 'Loading...';
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{xs: 'column', md: 'row'}}>
+      <Box flex={1}>
+        <Box sx={{width: '100%', position: 'sticky', top: '86px' }}>
+          <ReactPlayer url={`https://www.youtube.com/watch?v=${id}` } className="react-player" controls />
+          <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
+            {VideoDetial?.snippet?.title}
+          </Typography>
+          <Stack direction="row" justifyContent="space-between" sx={{color: '#fff'}} py={1} px={2}>
+          <Link to={`/channel/${VideoDetial?.snippet?.channelId}`}>
+          <Typography variant={{ sm: 'subtitle1', md: 'h6'}} color='#fff'>
+            {VideoDetial?.snippet?.channelTitle}
+            <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px'}}/>
+          </Typography>
+          </Link>
+          <Stack direction="row" gap="20px" alignItems="center">
+            <Typography variant='body1' sx={{opacity: 0.7 }}>
+               {parseInt(VideoDetial?.statistics?.viewCount).toLocaleString()} Views
+            </Typography>
+            <Typography variant='body1' sx={{opacity: 0.7 }}>
+               {parseInt(VideoDetial?.statistics?.likeCount).toLocaleString()} Likes
+            </Typography>
+          </Stack>
+          </Stack>
+        </Box>
+      </Box>
+      <Box px={2} py={{md: 1, xs: 5}} justifyContent="center" alignItems="center">
+      <Videos videos={videos} direction="column"/>
+      </Box>
+      </Stack>
+    </Box>
+  )
+}
+
+export default VideoDetial
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+export { default as Navbar } from './Navbar'
+export { default as Feed } from './Feed'
+export { default as ChannelDetial } from './ChannelDetial'
+export { default as SearchFeed } from './SearchFeed'
+export { default as VideoDetial } from './VideoDetial'
+export { default as SearchBar } from './SearchBar'
+export { default as SideBar} from './SideBar'
+export { default as Videos} from './Videos.jsx'
+export { default as ChannelCard} from './ChannelCard'
+export { default as VideoCard} from './VideoCard'
+
+
+
+

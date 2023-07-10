7d3c46d8ca1b6f7cdc7127b4e07ee21159d66dcf
Lakshmi prasanth S <114032664+prasanth-sasuke@users.noreply.github.com>
2023-07-10 21:01:32 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
+  { name: 'sk yaman yt', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
